c90d9924382d5d2a999bb37f43124807dc846fca
Lee Ter Yi <lteryi@gmail.com>
2020-04-12 12:53:07 +0800

    commit
    commit

--- myfirstreact/README.md
+++ myfirstreact/README.md
@@ -1,4 +1,5 @@
 ## EPCOMBB Web Specialist ReactJS Lesson
 
 1. Do not share to other
-2. Please create for member
+2. Please create for member 
+3. adasdas
--- myfirstreact/src/TableInfo.js
+++ myfirstreact/src/TableInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import DataTable from "react-data-table-component";
 import {
-Spinner,
+  Spinner,
   Button
 } from 'react-bootstrap';
 
@@ -14,7 +14,7 @@ class TableInfo extends React.Component {
       isLoaded: false,
       items: [],
       columns: [
-        
+
         {
           name: "Country",
           selector: "Country",
@@ -40,71 +40,71 @@ class TableInfo extends React.Component {
           selector: "Active",
           sortable: true,
         },
-          {
-            name: "Deaths",
-            selector: "Deaths",
-            sortable: true,
-          },
-         
+        {
+          name: "Deaths",
+          selector: "Deaths",
+          sortable: true,
+        },
+
       ],
     };
   }
- 
+
   retrieve_data() {
-       this.setState({
-         isLoaded: !this.state.isLoaded,
-       });
-      fetch('https://api.covid19api.com/live/country/malaysia/status/confirmed')
-        .then(res => res.json())
-        .then(
-          (result) => {
-            console.log(result)
-            this.setState({
-              isLoaded: true,
-              items: result,
-            });
-          },
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
+    this.setState({
+      isLoaded: !this.state.isLoaded,
+    });
+    fetch('https://api.covid19api.com/live/country/malaysia/status/confirmed')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          this.setState({
+            isLoaded: true,
+            items: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
   componentDidMount() {
-        this.retrieve_data = this.retrieve_data.bind(this);
+    this.retrieve_data = this.retrieve_data.bind(this);
 
     this.retrieve_data()
   }
-  componentDidUpdate(prevState){
-      console.log(prevState)
+  componentDidUpdate(prevState) {
+    console.log(prevState)
   }
   render() {
-    const { error, isLoaded, items,columns } = this.state;
+    const { error, isLoaded, items, columns } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div><Spinner animation="border" role="status">
-  <span className="sr-only">Loading...</span>
-</Spinner></div>;
+        <span className="sr-only">Loading...</span>
+      </Spinner></div>;
     } else {
       return (
         <div>
-        <Button onClick={this.retrieve_data}>Refresh</Button>
-        <DataTable
-          title="Employee"
-          columns={columns}
-          data={items}
-          // fixedHeader={true}
-          // pagination={true}
-          // paginationPerPage={10}
-          highlightOnHover={true}
-          pointerOnHover={true}
+          <Button onClick={this.retrieve_data}>Refresh</Button>
+          <DataTable
+            title="Employee"
+            columns={columns}
+            data={items}
+            // fixedHeader={true}
+            // pagination={true}
+            // paginationPerPage={10}
+            highlightOnHover={true}
+            pointerOnHover={true}
           // theme={'default'}
-        ></DataTable>
+          ></DataTable>
         </div>
-        
+
         // <table>
         //   {items.map(item => (
         //     <tr key={item.id}>
