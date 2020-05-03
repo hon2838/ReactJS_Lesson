a932a6ba30768e3507611b155942c33149ab93a8
Lee Ter Yi <lteryi@gmail.com>
2020-05-03 11:09:35 +0800

    Reorganise Folder Structure
    Signed-off-by: Lee Ter Yi <lteryi@gmail.com>

--- lesson04/src/pages/TopicPage.js
+++ lesson04/src/pages/TopicPage.js
@@ -2,30 +2,36 @@ import React from "react";
 import TopMenuBar from "../components/Menu"
 import {Route, Switch, useParams, useRouteMatch} from "react-router-dom";
 
-function TopicPage() {
+function TopicPageLanding() {
     let match = useRouteMatch();
 
     return (
         <div>
             <TopMenuBar/>
             <h2>Topics</h2>
+            <ul>
+                <li><a href="/Topics/100">100</a></li>
+                <li><a href="/Topics/10">10</a></li>
+
+            </ul>
 
             <Switch>
                 <Route path={`${match.path}/:topicId`}>
-                    <Topic/>
+                    <TopicPage/>
                 </Route>
                 <Route path={match.path}>
                     <h3>Please select a topic.</h3>
                 </Route>
             </Switch>
+
         </div>
     );
 
 }
 
-function Topic() {
+function TopicPage() {
     let {topicId} = useParams();
     return <h3>Requested topic ID: {topicId}</h3>;
 }
 
-export default TopicPage;
+export default TopicPageLanding;
